845fe7f5f7688f3bd91248f04a6972e0fd4d6880
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-06-04 15:03:09 +0200

    la list des noms des titres est faite !

--- script.js
+++ script.js
@@ -1,3 +1,73 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    console.log('script')
+import codes from './codes.js'
+const c = new codes()
+
+
+document.addEventListener('DOMContentLoaded', async() => {
+    const token = await getToken()
+    console.log(token)
+    // console.log('pivjpdjip')
+
+    let input = document.getElementById('input')
+    let button = document.getElementById('button')
+    
+
+
+    button.addEventListener('click', () => {
+        let id = input.value.split('/playlist/')[1].split('?')[0]
+        getPlaylist(id, token)
+    })
+
+
+
+
+
+
 })
+
+
+
+async function getPlaylist(id, token){
+    fetch(`https://api.spotify.com/v1/playlists/${id}/tracks`, {
+        mode: 'cors',
+        headers: {
+            Authorization: 'Bearer ' + token,
+        },
+    }).then(r =>r.json()
+    ).then(j=>{
+        j.items.forEach(elem => {
+            trackByTrack(elem.track)  
+        });
+    
+    }).catch(e => console.log(e))
+}
+
+
+
+
+
+
+async function trackByTrack(arg){
+    console.log(arg)
+
+    let p = document.createElement('p')
+    document.getElementById('tracks').appendChild(p)
+    p.innerHTML = arg.name
+}
+
+
+async function getToken(){
+
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(c.id + ':' + c.secretId),
+            // 'Access-Control-Allow-Origin':'*'
+        },
+        body: 'grant_type=client_credentials',
+        mode: 'cors'
+    });
+
+    const data = await result.json();
+    return data.access_token;
+}
